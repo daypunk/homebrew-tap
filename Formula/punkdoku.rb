2f817cfdf48b2f80a99a89d4476a85b835809bf2
daypunk <daehee3853@gmail.com>
2025-08-14 18:33:21 +0900

    Fix URL version mismatch - update URLs to v1.0.7

--- Formula/punkdoku.rb
+++ Formula/punkdoku.rb
@@ -5,16 +5,16 @@ class Punkdoku < Formula
   
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/daypunk/punkdoku/releases/download/v1.0.0/punkdoku-macos-arm64"
+        url "https://github.com/daypunk/punkdoku/releases/download/v1.0.7/punkdoku-macos-arm64"
         sha256 "8342e31bedfd1620676abaeca5d758b68979a1f9d20e542bb961244f9e77f4ae"
       else
-        url "https://github.com/daypunk/punkdoku/releases/download/v1.0.0/punkdoku-macos-amd64"
+        url "https://github.com/daypunk/punkdoku/releases/download/v1.0.7/punkdoku-macos-amd64"
         sha256 "8342e31bedfd1620676abaeca5d758b68979a1f9d20e542bb961244f9e77f4ae"
       end
     end
   
     on_linux do
-      url "https://github.com/daypunk/punkdoku/releases/download/v1.0.0/punkdoku-linux"
+      url "https://github.com/daypunk/punkdoku/releases/download/v1.0.7/punkdoku-linux"
       sha256 "8342e31bedfd1620676abaeca5d758b68979a1f9d20e542bb961244f9e77f4ae"
     end
   
